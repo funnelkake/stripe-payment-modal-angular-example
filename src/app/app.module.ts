11b4ef9f0a9cea54b1394e60a9fc3ccdc60fcb33
Andrew Zelenets <andrew.zelenets@gmail.com>
2021-03-26 11:23:25 +0200

    add Quantity handling example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSelectModule } from "@angular/material/select";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     MatCardModule,
+    MatSelectModule,
     MatButtonModule,
     BrowserAnimationsModule,
   ],
