67915d335d874b81548d9f01d22fa05b3359e4b8
Andrew Zelenets <andrew.zelenets@gmail.com>
2021-03-26 11:23:25 +0200

    add Quantity handling example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { AfterViewInit, Component } from '@angular/core';
 import { get } from 'scriptjs';
 import { environment } from 'src/environments/environment';
 
-// use this line instead for local
-// import { environment } from 'src/environments/environment';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +9,15 @@ import { environment } from 'src/environments/environment';
 })
 export class AppComponent implements AfterViewInit {
   title = 'Payment Modal Angular Demo';
+  paymentAmountQuantity = "1";
+  paymentPriceIdQuantity = "1";
+  subscriptionPaymentQuantity = "1";
+
+  onQuantityChanged(event, openModalSelector) {
+    const openModalElement = document.querySelector(`button${openModalSelector}`);
+    openModalElement.setAttribute("data-product-quantity", event.value);
+  }
+
   ngAfterViewInit() {
     get(
       `${environment.ANGULAR_APP_PAYMENT_MODAL_API_HOST_URL}/api/stripe_modals/script.js?siteId=${environment.ANGULAR_APP_PAYMENT_MODAL_SITE_ID}`,
